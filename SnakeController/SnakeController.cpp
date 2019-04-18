87269289e88781d14c8251cc892f61dd6a78179a
Amer <academy.19.student.ext@nokia.com>
2019-04-18 19:14:59 +0200

    Zadanie 2 - zmiana na if i static cast

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -215,22 +215,26 @@ Controller::Segment Controller::getNewHead() const
 
 void Controller::receive(std::unique_ptr<Event> e)
 {
-    try {
-        handleTimePassed(*dynamic_cast<EventT<TimeoutInd> const&>(*e));
-    } catch (std::bad_cast&) {
-        try {
-            handleDirectionChange(*dynamic_cast<EventT<DirectionInd> const&>(*e));
-        } catch (std::bad_cast&) {
-            try {
-                handleFoodPositionChange(*dynamic_cast<EventT<FoodInd> const&>(*e));
-            } catch (std::bad_cast&) {
-                try {
-                    handleNewFood(*dynamic_cast<EventT<FoodResp> const&>(*e));
-                } catch (std::bad_cast&) {
-                    throw UnexpectedEventException();
-                }
-            }
-        }
+    auto checkMsg = e->getMessageId();
+
+    if(checkMsg == TimeoutInd::MESSAGE_ID)
+    {
+       handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
+    }
+    else  if(checkMsg == DirectionInd::MESSAGE_ID)
+    {
+        handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
+    }
+    else  if(checkMsg == FoodInd::MESSAGE_ID)
+    {
+        handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
+    }
+    else  if(checkMsg == FoodResp::MESSAGE_ID)
+    {
+        handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+    } else
+    {
+        throw UnexpectedEventException();
     }
 }
 
