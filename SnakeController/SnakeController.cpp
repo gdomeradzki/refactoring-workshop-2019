7a7cb91fa418cd918a9cfba6c987cf6ad6161a6e
acadstud <academy.3.student.ext@nokia.com>
2019-04-18 19:58:13 +0200

    Zadanie 3

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -150,6 +150,11 @@ void Controller::handleNewFood(const FoodResp& requestedFood)
     m_foodPosition = std::make_pair(requestedFood.x, requestedFood.y);
 }
 
+void Controller::handlePause(const PauseInd& pause)
+{
+
+}
+
 bool Controller::doesCollideWithSnake(const Controller::Segment &newSegment) const
 {
     for (auto segment : m_segments) {
@@ -214,13 +219,16 @@ Controller::Segment Controller::getNewHead() const
 }
 
 void Controller::receive(std::unique_ptr<Event> e)
-{
+{     
+    if(!isPaused)
     switch(e->getMessageId())
     {
+        case PauseInd::MESSAGE_ID: return handlePause(*static_cast<EventT<PauseInd> const&>(*e));
         case TimeoutInd::MESSAGE_ID: return handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
         case DirectionInd::MESSAGE_ID: return handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
         case FoodInd::MESSAGE_ID: return handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
         case FoodResp::MESSAGE_ID: return handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+
         default: throw UnexpectedEventException();
     };
 }
