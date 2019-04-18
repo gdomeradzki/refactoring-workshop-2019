88b0d16ac951281cceb8ae1e947b00a3793492db
academy-36-student <academu.36.student.ext@nokia.com>
2019-04-18 19:12:35 +0200

    Zadanie 2

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -215,22 +215,25 @@ Controller::Segment Controller::getNewHead() const
 
 void Controller::receive(std::unique_ptr<Event> e)
 {
-    try {
-        handleTimePassed(*dynamic_cast<EventT<TimeoutInd> const&>(*e));
-    } catch (std::bad_cast&) {
-        try {
-            handleDirectionChange(*dynamic_cast<EventT<DirectionInd> const&>(*e));
-        } catch (std::bad_cast&) {
-            try {
-                handleFoodPositionChange(*dynamic_cast<EventT<FoodInd> const&>(*e));
-            } catch (std::bad_cast&) {
-                try {
-                    handleNewFood(*dynamic_cast<EventT<FoodResp> const&>(*e));
-                } catch (std::bad_cast&) {
-                    throw UnexpectedEventException();
-                }
-            }
-        }
+    if( e -> getMessageId() == Snake::TimeoutInd::MESSAGE_ID)
+    {
+        handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
+    }
+    else if(e -> getMessageId() == Snake::DirectionInd::MESSAGE_ID)
+    {
+         handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
+    }
+    else if (e -> getMessageId() == Snake::FoodInd::MESSAGE_ID)
+    {
+        handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
+    }
+    else if(e -> getMessageId() == Snake::FoodResp::MESSAGE_ID)
+    {
+        handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+    }
+    else
+    {
+        throw UnexpectedEventException();
     }
 }
 
