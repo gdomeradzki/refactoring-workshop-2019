c7b963c5142c7788df6c9bb87931e8f15b495249
Mateusz Macias <macias.mateusz94@gmail.com>
2019-04-18 20:01:32 +0200

    Zadanie 3

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -150,6 +150,12 @@ void Controller::handleNewFood(const FoodResp& requestedFood)
     m_foodPosition = std::make_pair(requestedFood.x, requestedFood.y);
 }
 
+void Controller::handlePause(const PauseInd& paused)
+{
+    paused = true;
+}
+
+
 bool Controller::doesCollideWithSnake(const Controller::Segment &newSegment) const
 {
     for (auto segment : m_segments) {
@@ -217,10 +223,12 @@ void Controller::receive(std::unique_ptr<Event> e)
 {
     switch(e->getMessageId())
     {
-        case TimeoutInd::MESSAGE_ID: return handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
-        case DirectionInd::MESSAGE_ID: return handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
+        case TimeoutInd::MESSAGE_ID: if (!paused) { return  handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e)); }
+        case DirectionInd::MESSAGE_ID: if (!paused) { return handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e)); }
         case FoodInd::MESSAGE_ID: return handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
         case FoodResp::MESSAGE_ID: return handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+        case PauseInd::MESSAGE_ID: return handlePause(*static_cast<EventT<PauseInd> const&>(*e));
+
         default: throw UnexpectedEventException();
     };
 }
