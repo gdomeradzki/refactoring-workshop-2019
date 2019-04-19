9b138483961b7aa237cf00f0de1050ec2f53015e
Tomasz Mandyk <academy.31.student.ext@nokia.com>
2019-04-19 23:18:12 +0200

    Zadanie 4

--- SnakeController/SnakeWorld.cpp
+++ SnakeController/SnakeWorld.cpp
@@ -1,11 +1,16 @@
 #include "SnakeWorld.hpp"
+#include "IPort.hpp"
+#include "EventT.hpp"
+#include "SnakeSegments.hpp"
 
 namespace Snake
 {
 
-World::World(Dimension dimension, Position food)
+World::World(Dimension dimension, Position food, IPort& displayPort, IPort& foodPort)
     : m_foodPosition(food),
-      m_dimension(dimension)
+      m_dimension(dimension),
+      m_displayPort(displayPort),
+      m_foodPort(foodPort)
 {}
 
 void World::setFoodPosition(Position position)
@@ -22,4 +27,44 @@ bool World::contains(Position position) const
 {
     return m_dimension.isInside(position);
 }
+
+void World::updateFoodPosition(Position position, Segments& m_segments)
+{
+    updateFoodPositionAfterClearPolicy(position, std::bind(&World::sendClearOldFood, this), m_segments);
+}
+
+void World::updateFoodPositionAfterClearPolicy(Position position, std::function<void()> clearPolicy, Segments& m_segments)
+{
+    if (m_segments.isCollision(position) or not contains(position)) {
+        m_foodPort.send(std::make_unique<EventT<FoodReq>>());
+        return;
+    }
+
+    clearPolicy();
+    sendPlaceNewFood(position);
+}
+
+void World::sendClearOldFood()
+{
+    auto foodPosition = getFoodPosition();
+
+    DisplayInd clearOldFood;
+    clearOldFood.position = foodPosition;
+    clearOldFood.value = Cell_FREE;
+
+    m_displayPort.send(std::make_unique<EventT<DisplayInd>>(clearOldFood));
+}
+
+void World::sendPlaceNewFood(Position position)
+{
+    setFoodPosition(position);
+
+    DisplayInd placeNewFood;
+    placeNewFood.position = position;
+    placeNewFood.value = Cell_FOOD;
+
+    m_displayPort.send(std::make_unique<EventT<DisplayInd>>(placeNewFood));
+}
+
+
 } // namespace Snake
--- SnakeController/SnakeWorld.hpp
+++ SnakeController/SnakeWorld.hpp
@@ -1,23 +1,40 @@
 #pragma once
 
+#include <functional>
+
 #include "SnakePosition.hpp"
 #include "SnakeDimension.hpp"
+
+class Event;
+class IPort;
 
 namespace Snake
 {
 
+class Segments;
 class World
 {
 public:
-    World(Dimension dimension, Position food);
+    World(Dimension dimension, Position food, IPort& displayPort, IPort& foodPort);
 
     void setFoodPosition(Position position);
     Position getFoodPosition() const;
 
     bool contains(Position position) const;
+
+    // void updateFoodPosition(Position position, std::function<void()> clearPolicy, Segments& m_segments);
+    void updateFoodPosition(Position position, Segments& m_segments);
+    void updateFoodPositionAfterClearPolicy(Position position, std::function<void()> clearPolicy, Segments& m_segments);
+    void sendClearOldFood();
+    void sendPlaceNewFood(Position position);
+
+
 private:
     Position m_foodPosition;
     Dimension m_dimension;
+
+    IPort& m_displayPort;
+    IPort& m_foodPort;
 };
 
 } // namespace Snake
